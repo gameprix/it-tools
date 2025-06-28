eeea527829c7ba4e7ca6f7d033659fbdee88a129
Dhwanik Panchal <panchaldhwanik9j@gmail.com>
2025-06-28 13:20:23 +0530

    feat(cli-command-editor): Added a new tool: CLI command editor

---
+++ src/tools/cli-command-editor/cli-command-editor.e2e.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Tool - Cli command editor', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/cli-command-editor');
+  });
+
+  test('Has correct title', async ({ page }) => {
+    await expect(page).toHaveTitle('Cli command editor - IT Tools');
+  });
+
+  test('', async ({ page }) => {
+
+  });
+});
---
+++ src/tools/cli-command-editor/cli-command-editor.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+  import * as service from "./cli-command-editor.service"
+  const inputCommand = ref("");
+  const options = computed(() => service.extractOptions(inputCommand.value));
+  const optionsObject = computed(() => service.buildOptionsObject(options.value));
+  const optionsInput = ref<{ [k: string]: string }>(optionsObject.value);
+  let command = ref("");
+</script>
+
+<template>
+  <c-card>
+    <n-grid x-gap="12" y-gap="12" cols="1 600:3">
+      <n-gi span="2">
+        <c-input-text
+          v-model:value="inputCommand"
+          label-position="left"
+          label-width="130px"
+          :label="$t('tools.cli-command-editor.command')"
+          :aria-label="$t('tools.cli-command-editor.command')"
+          :placeholder="$t('tools.cli-command-editor.placeholder')"
+          :aria-placeholder="$t('tools.cli-command-editor.placeholder')"
+          raw-text
+          
+          @update:value="() => {command = inputCommand}"
+        />
+        <div v-for="option in options" flex justify-center>
+          <c-input-text
+            v-model:value="optionsInput[option]"
+            label-position="left"
+            label-width="130px"
+            label-align="left"
+            :label="service.sanitizeOption(option)"
+            :aria-label="service.sanitizeOption(option)"
+            :placeholder="service.sanitizeOption(option)"
+            :aria-placeholder="service.sanitizeOption(option)"
+            @update:value="() => {command = service.buildEditedCommand(optionsInput, optionsObject, inputCommand)}"
+            mt-6
+          />
+        </div>
+
+        <c-text-copyable
+          :value="command"
+          v-if="command"
+          font-mono
+          :show-icon="false"
+          mt-6 
+        />
+      </n-gi>
+    </n-grid>
+  </c-card>
+</template>
+
+<style lang="less" scoped>
+</style>
